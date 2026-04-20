7183787f906ae2a362d8949374188eac944a9a22
Saksi <np05cp4s240097@iic.edu.np>
2026-04-20 23:10:36 +0545

    fix

--- client/src/routes.jsx
+++ client/src/routes.jsx
@@ -20,10 +20,10 @@ import NotFound from "./pages/Notfound";
 import Register from "./pages/Register";
 import { protectedLoader } from "./utils/authLoader";
 import CampaignChat from "./features/chat/Campaignchat";
-import Client from './layout/Client';
-import Common from "./layout/Common";
-import Dashboard from "./layout/Dashboard";
-import RootLayout from "./layout/Root";
+import Client from "./layout/Client.jsx";
+import Common from "./layout/Common.jsx";
+import Dashboard from "./layout/Dashboard.jsx";
+import RootLayout from "./layout/Root.jsx";
 
 export const router = createBrowserRouter([
   {
